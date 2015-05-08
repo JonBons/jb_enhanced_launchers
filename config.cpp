6a6f20659e81d76e7eefc10591fde9a4be05ea56
Matthew Graham <me@mattgraham.me>
2015-05-07 22:36:38 -0500

    Fixed typo which broke HE against air/inf and changed it so that AP/HE can be used against anything
    Fixed typo which broke HE against air/inf and changed it so that AP/HE can be used against anything unless you use the NOMOD magazines.

--- config.cpp
+++ config.cpp
@@ -17,15 +17,19 @@ class CfgAmmo
 
   class R_PG32V_F: RocketBase
   {
+    airLock = 1;
+    allowAgainstInfantry = 1;
     soundFly[] = {"A3\Sounds_F\weapons\Rockets\rocket_fly_1",6,1.5,400};
     whistleDist = 24;
   };
-  class R_PG32V_AI_F: R_PG32V_F {
-    airLock = 1;
-    allowAgainstInfantry = 1;
-  };
 
   class R_TBG32V_F: R_PG32V_F {};
+
+  class R_PG32V_NOMOD_F: R_PG32V_F {
+    airLock = 0;
+    allowAgainstInfantry = 0;
+  };
+
   class R_TBG32V_NOMOD_F: R_TBG32V_F {
     airLock = 0;
     allowAgainstInfantry = 0;
@@ -41,11 +45,11 @@ class CfgMagazines {
   class RPG32_F: CA_LauncherMagazine {};
   class RPG32_HE_F: RPG32_F {};
 
-  class RPG32_AI_F: RPG32_F
+  class RPG32_NOMOD_F: RPG32_F
   {
-    ammo = "R_PG32V_AI_F";
-    descriptionShort = "Type: RPG-42 rocket (AI Mod)<br />Rounds: 1<br />Used in: RPG-42";
-    displayName = "RPG-42 Rocket (AI Mod)";
+    ammo = "R_PG32V_NOMOD_F";
+    descriptionShort = "Type: RPG-42 rocket (No Mod)<br />Rounds: 1<br />Used in: RPG-42";
+    displayName = "RPG-42 Rocket (No Mod)";
   };
 
   class RPG32_HE_NOMOD_F: RPG32_HE_F
